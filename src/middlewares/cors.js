837ce0f1278d8369b9462c9571d3f4f74f649d9d
Levisel <fvelascoreyes@gmail.com>
2025-03-23 09:36:24 -0500

    comentario cors

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -4,5 +4,5 @@ export const corsMiddleware = () => cors({
   origin: 'http://localhost:5173', // Origen de la petición (Esta con el puerto de la app de VUE.JS) 
   credentials: true,               // Permite el envío de credenciales (cookies)
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  allowedHeaders: 'Content-Type,Authorization,internal-id', // Cabeceras permitidas
+  allowedHeaders: 'Content-Type,Authorization,internal-id', //No borrar la cabecera internal-id, es necesaria para el manejo de auditoría
 });
