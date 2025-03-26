8e4f6473144f51baaeeb5368039ccef14d5ef2db
TefoV3 <tefouni12@gmail.com>
2025-03-26 11:44:29 -0500

    consultoriosjuridicosBack

--- src/routes/social_work_routes.js
+++ src/routes/social_work_routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { SocialWorkController } from "../controllers/SocialWorkController.js";
+import { authMiddleware } from "../middlewares/auth.js";
 
 export const SocialWorkRouter = Router();
 
 SocialWorkRouter.get("/social-work", SocialWorkController.getAll);
 SocialWorkRouter.get("/social-work/:id", SocialWorkController.getById);
 SocialWorkRouter.get("/social-work/user/:initCode", SocialWorkController.getUserIdBySocialWork);
-SocialWorkRouter.post("/social-work", SocialWorkController.create);
+SocialWorkRouter.post("/social-work", authMiddleware, SocialWorkController.create);
 SocialWorkRouter.put("/social-work/:id", SocialWorkController.update);
 SocialWorkRouter.delete("/social-work/:id", SocialWorkController.delete);
 
