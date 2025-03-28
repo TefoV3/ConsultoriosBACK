49c83cfe589a43bf7b6a76441eb5ae075919aa4b
WMBECERRA <wmbecerra@puce.edu.ec>
2025-03-27 20:29:40 -0500

    Commit

--- src/index.js
+++ src/index.js
@@ -3,32 +3,32 @@ import { sequelize } from "./database/database.js";
 import { PORT } from "./config.js";
 
 
-// async function main(){
-//    try {
-//       await sequelize.sync({ alter: false });  // true si hay cambios en el schemas, false si no hay cambios
-//       console.log("✅ Base de datos sincronizada correctamente en Supabase");
+async function main(){
+   try {
+      await sequelize.sync({ alter: false });  // true si hay cambios en el schemas, false si no hay cambios
+      console.log("✅ Base de datos sincronizada correctamente en Supabase");
 
-//       app.listen(PORT, () => {
-//          console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
-//       });
+      app.listen(PORT, () => {
+         console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
+      });
 
-//    } catch (error) {
-//       console.error("❌ Error al iniciar el servidor:", error);
-//    }
-// }
-//  main();
+   } catch (error) {
+      console.error("❌ Error al iniciar el servidor:", error);
+   }
+}
+ main();
 
 //USO LOCAL
 
-async function main(){
-   try { 
-      await sequelize.sync();
-      app.listen(3000, () => {
-         console.log("Server running on port 3000")
-      })
-   } catch (error) {
-      console.error("Error starting server: ", error)
-   }
-}
+// async function main(){
+//    try { 
+//       await sequelize.sync();
+//       app.listen(3000, () => {
+//          console.log("Server running on port 3000")
+//       })
+//    } catch (error) {
+//       console.error("Error starting server: ", error)
+//    }
+// }
 
-main()
+// main()
