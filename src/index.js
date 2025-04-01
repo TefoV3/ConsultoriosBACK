cb3f77cc048308da5210c1eb7237b6eb421614be
Levisel <fvelascoreyes@gmail.com>
2025-03-31 19:34:20 -0500

    error rutas corregido

--- src/index.js
+++ src/index.js
@@ -3,34 +3,34 @@ import { sequelize } from "./database/database.js";
 import { PORT } from "./config.js";
 
 
-async function main(){
-   try {
-      await sequelize.sync({ alter: false });  // true si hay cambios en el schemas, false si no hay cambios
-      console.log("✅ Base de datos sincronizada correctamente en Supabase");
+// async function main(){
+//    try {
+//       await sequelize.sync({ alter: false });  // true si hay cambios en el schemas, false si no hay cambios
+//       console.log("✅ Base de datos sincronizada correctamente en Supabase");
 
-      app.listen(PORT, () => {
-         console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
-      });
+//       app.listen(PORT, () => {
+//          console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
+//       });
 
-   } catch (error) {
-      console.error("❌ Error al iniciar el servidor:", error);
-   }
-}
- main();
+//    } catch (error) {
+//       console.error("❌ Error al iniciar el servidor:", error);
+//    }
+// }
+//  main();
 
 //USO LOCAL
 
-// import "./schemas/schedules_tables/associations.js";
+import "./schemas/schedules_tables/associations.js";
 
-// async function main(){
-//    try { 
-//       await sequelize.sync(/*{force: true}*/);
-//       app.listen(3000, () => {
-//          console.log("Server running on port 3000")
-//       })
-//    } catch (error) {
-//       console.error("Error starting server: ", error)
-//    }
-// }
+async function main(){
+   try { 
+      await sequelize.sync(/*{force: true}*/);
+      app.listen(3000, () => {
+         console.log("Server running on port 3000")
+      })
+   } catch (error) {
+      console.error("Error starting server: ", error)
+   }
+}
 
-// main()
+main()
--- src/models/ActivityRecordModel.js
+++ src/models/ActivityRecordModel.js
@@ -1,7 +1,7 @@
 import { AuditModel } from "../models/AuditModel.js";
 import { ActivityRecord } from "../schemas/Activity_Record.js";
 import { sequelize } from "../database/database.js";
-import { getUserId } from './sessionData.js';
+import { getUserId } from '../sessionData.js';
 
 export class ActivityRecordModel {
     static async getAll() {
