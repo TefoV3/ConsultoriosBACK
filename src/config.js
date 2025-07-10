f6aa9de88f2095daf53c1068ebf69a7aa4548190
Levisel <fvelascoreyes@gmail.com>
2025-07-10 16:04:45 -0500

    fix

--- src/config.js
+++ src/config.js
@@ -9,6 +9,6 @@ export const{
     EMAIL_PORT = 465, // Puerto SMTP: 587 (TLS), 465 (SSL), 25 (no seguro)
     EMAIL_SECURE = true, // true para puerto 465 (SSL), false para 587 (TLS) o 25
     EMAIL_USER = "email@gmail.com", // Correo electrónico donde se enviarán las notificaciones
-    EMAIL_PASS = "contraseña de desarrollodor", // Contraseña de aplicación para el correo electrónico
+    EMAIL_PASS = "contraseña de desarrollador", // Contraseña de aplicación para el correo electrónico
     EMAIL_TLS_REJECT_UNAUTHORIZED = false, // Para certificados auto-firmados o desarrollo
 } = process.env;
